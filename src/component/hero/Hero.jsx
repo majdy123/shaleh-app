983a413cf64981eda1ff8b73226dbc9ab12da6ae
baraaaboshi123 <baraa5hfg@gmail.com>
2023-01-14 18:54:31 +0200

    baraa-4

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -1,9 +1,76 @@
 import React, { Component } from 'react'
 import './hero.css'
+import Img1 from './img1.jpg'
+import Img2 from './img2.jpg'
+import Img3 from './img3.jpg'
+import Img4 from './img4.jpg'
+import Img5 from './img5.jpg'
+import Img6 from './img6.jpg'
+
 export default class Hero extends Component {
   render() {
     return (
-      <div>Hero</div>
+      <>
+      <center>
+        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="2000">
+    <img src={Img1} className="d-block w-100 carosel-img" alt="صورة شاليه"/>
+    <div class="carousel-caption d-none d-md-block">
+    <button className='bg-primary'>احجز الشاليه الخاص بك الان</button>
+
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000">
+    <img src={Img2} className="d-block w-100 carosel-img" alt="صورة شاليه"/>
+    <div class="carousel-caption d-none d-md-block">
+    <button className='bg-primary '>احجز الشاليه الخاص بك الان</button>
+
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000">
+    <img src={Img3} className="d-block w-100 carosel-img" alt="صورة شاليه"/>
+    <div class="carousel-caption d-none d-md-block">
+    <button className='bg-primary'>احجز الشاليه الخاص بك الان</button>
+
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000">
+    <img src={Img4} className="d-block w-100 carosel-img" alt="صورة شاليه"/>
+    <div class="carousel-caption d-none d-md-block">
+    <button className='bg-primary '>احجز الشاليه الخاص بك الان</button>
+
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000">
+    <img src={Img5} className="d-block w-100 carosel-img" alt="صورة شاليه"/>
+    <div class="carousel-caption d-none d-md-block">
+    <button className='bg-primary hover bttn'>احجز الشاليه الخاص بك الان</button>
+
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000">
+    <img src={Img6} className="d-block w-100 carosel-img" alt="صورة شاليه"/>
+    <div class="carousel-caption d-none d-md-block">
+    <button className='bg-primary '>احجز الشاليه الخاص بك الان</button>
+
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+      </center>
+     
+
+      </>
     )
   }
-}
+  }
+
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 import './home.css'
+import Hero from '../hero/Hero'
+import About from '../about/About'
 export default class Home extends Component {
   render() {
     return (
       <div className='home_page'>
         <Header />
+        <About/>
+        <Hero/>
         <Footer />
       </div>
     )

8b68a55a7c17a38227cc872055d49030d145dd45
majdy <majdysabra4@gmail.com>
2023-01-14 13:36:49 +0200

    Majdy

--- src/component/notfound/NotFound.jsx
+++ src/component/notfound/NotFound.jsx
@@ -4,36 +4,41 @@ import './notfound.css'
 export default class notfound extends Component {
   render() {
     return (
-        <div class="container">
+      <div className='button_backhome'>
+        <div className="container">
             <script src="/script/gsap.min.js"></script>
-        <h1 class="first-four">4</h1>
-        <div class="cog-wheel1">
-            <div class="cog1">
-              <div class="top"></div>
-              <div class="down"></div>
-              <div class="left-top"></div>
-              <div class="left-down"></div>
-              <div class="right-top"></div>
-              <div class="right-down"></div>
-              <div class="left"></div>
-              <div class="right"></div>
+        <h1 className="first-four">4</h1>
+        <div className="cog-wheel1">
+            <div className="cog1">
+              <div className="top"></div>
+              <div className="down"></div>
+              <div className="left-top"></div>
+              <div className="left-down"></div>
+              <div className="right-top"></div>
+              <div className="right-down"></div>
+              <div className="left"></div>
+              <div className="right"></div>
           </div>
         </div>
         
-        <div class="cog-wheel2"> 
-          <div class="cog2">
-              <div class="top"></div>
-              <div class="down"></div>
-              <div class="left-top"></div>
-              <div class="left-down"></div>
-              <div class="right-top"></div>
-              <div class="right-down"></div>
-              <div class="left"></div>
-              <div class="right"></div>
+        <div className="cog-wheel2"> 
+          <div className="cog2">
+              <div className="top"></div>
+              <div className="down"></div>
+              <div className="left-top"></div>
+              <div className="left-down"></div>
+              <div className="right-top"></div>
+              <div className="right-down"></div>
+              <div className="left"></div>
+              <div className="right"></div>
           </div>
         </div>
-       <h1 class="second-four">4</h1>
-        <p class="wrong-para">الصفحة غير موجودة او غير صحيحة</p>
+       <h1 className="second-four">4</h1>
+        <p className="wrong-para">الصفحة غير موجودة او غير صحيحة</p>
+        </div>
+          <a href="/home">
+            <button className="btn btn-primary btn-lg">الصفحة الرئيسية</button>
+          </a>
       </div>
     )
   }
